c153477585132f1bf8217290584085da59187acb
Rohit Chand <rohitchand@Rohits-MacBook-Air.local>
2025-01-01 23:12:53 +0530

    Second Class Homework

---
+++ Second/App.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading= React.createElement("h1",{},"Namaste Everyone")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
